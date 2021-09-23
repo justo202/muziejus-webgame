6d38fb688cf7f90dda9e288d902e6c554c134807
justo202 <justaslabeikis23@gmail.com>
2021-09-23 15:04:43 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './DragAPI';
-
+import { HashRouter as Router } from "react-router-dom"
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
